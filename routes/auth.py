e94e358c997f3ee1b3c37e75a49d58f65ca85277
Tanu <g.tanushree1511@gmail.com>
2026-08-07 20:58:41 +0530

    like i connected everything

--- routes/auth.py
+++ routes/auth.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request
+from models.user import User
+from extensions import db, bcrypt
 auth = Blueprint("auth", __name__)
 
 
@@ -13,8 +14,28 @@ def choose_register():
     return render_template("choose_register.html")
 
 
-@auth.route("/register/trekker")
+@auth.route("/register/trekker", methods=["GET", "POST"])
 def register_trekker():
+
+    if request.method == "POST":
+
+        name = request.form.get("name")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        password = request.form.get("password")
+        hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
+        new_user = User(
+            name=name,
+            email=email,
+            phone=phone,
+            password=hashed_password,
+            role="trekker",
+            approved=True,
+            blacklisted=False
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        print("New trekker registered:", new_user)
     return render_template("register_trekker.html")
 
 
