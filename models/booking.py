bc0b2809196cda63a43b233b03afb55d067f6776
Tanu <g.tanushree1511@gmail.com>
2026-08-07 12:40:23 +0530

    booking module added

---
+++ models/booking.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from extensions import db
+
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id= db.Column(
+        db.Integer,
+        db.ForeignKey("user.id"),
+        nullable=False
+    )
+
+    trek_id = db.Column(
+        db.Integer,
+        db.ForeignKey("trek.id"),
+        nullable= False
+    )
+
+    booking_date = db.Column(
+        db.DateTime,
+        default =datetime.now
+    )
+    status = db.Column(
+        db.String(20),
+        default="Booked"
+    )
